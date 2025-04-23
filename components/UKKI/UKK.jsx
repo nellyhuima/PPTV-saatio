da35b1d2a868eb81bbb615ed6eba2600ca37b542
anisimaa <isla.anisimaa00@gmail.com>
2025-04-23 11:56:37 +0300

    continued ukk functionality -i

--- components/UKKI/UKK.jsx
+++ components/UKKI/UKK.jsx
@@ -10,17 +10,25 @@ const UKK = () => {
     setOpenIndex(openIndex === index ? null : index);
   };
 
+  const faqData = [
+    { question: "Kysmys 1", answer: "vastaus1." },
+    { question: "kysmys2", answer: "vastaus2." },
+    { question: "kysmys3", answer: "vastaus3" },
+  ];
+
   return (
     <div className="ukkpohja">
+      <div className="ukkcontent">
       <h1>UKK</h1>
       <section className="faq">
-        {[1, 2].map((item, index) => (
-          <div className="faq-item" key={index}>
-            <h3 onClick={() => toggleAnswer(index)}>Kysymys</h3>
-            {openIndex === index && <p className="answer">Vastaus</p>}
+      {faqData.map((item, index) => (
+          <div className="question" key={index}>
+            <h3 onClick={() => toggleAnswer(index)}>{item.question}</h3>
+            {openIndex === index && <p className="answer">{item.answer}</p>}
           </div>
         ))}
       </section>
+      </div>
     </div>
   );
 };
