763137e1d609ba03744a3cf789e9a357ac4232d5
nellyhuima <nelly.huima@icloud.com>
2025-04-26 17:06:43 +0300

    cchanged size of logo N

--- components/EtusivuN/Etusivu.jsx
+++ components/EtusivuN/Etusivu.jsx
@@ -4,7 +4,7 @@ import './Etusivu.css';
 const Etusivu = () => {
   return (
     <div className="etusivu">
-      <img src="/components/img/finalwhitelogo.png" alt='logo' height='300px'/>
+      <img src="/components/img/finalwhitelogo.png" alt='logo' height='350px' width='600px'/>
       <p className='etusivuQuote'>
         "Opiskelijoiden tukena jo vuodesta 1993"
       </p>
