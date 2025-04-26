f4b8e233172bdefb2e945a774f06586533d3c102
anisimaa <isla.anisimaa00@gmail.com>
2025-04-26 15:42:43 +0300

    new logo added -I

--- components/HakemusI/Hakemus.jsx
+++ components/HakemusI/Hakemus.jsx
@@ -7,11 +7,11 @@ const Hakemus = () => {
   return (
     <div className="lomake">
       <div className='lomakeContent'>
-        <h1>Hakemus</h1>
+        <h1 className="Htext">Hakemus</h1>
         <p className="text">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat, 
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
         </p>
         <button className="lomakebutton" onClick={() => navigate('/Lomake')}>Hae tästä!</button>
       </div>
