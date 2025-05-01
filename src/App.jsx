b40899fd46e16433bc3bdd4a0a19a6180da651d3
anisimaa <isla.anisimaa00@gmail.com>
2025-05-02 01:13:33 +0300

    final final touches -I

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import Home from "./Home";
 import Lomake from '../components/Lomake/Lomake'; 
 import './assets/fonts/font.css';
+import ScrollToTop from '../components/TopScroll/ScrollToTop';
 
 const App = () => {
 
   return (
+    <>
+    <ScrollToTop />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/lomake" element={<Lomake />} />
     </Routes>
+  </>
   );
 };
 
